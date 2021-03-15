ac04a5cf0e9017cf2f9d26f6ce50042d882349dd
Asmaa Tarek <asmaatarek93@gmail.com>
2021-03-15 18:06:42 +0200

    fix scroll in categories

--- bawabtalsharq/lib/Screens/all_categories_screen.dart
+++ bawabtalsharq/lib/Screens/all_categories_screen.dart
@@ -1,4 +1,3 @@
-import 'package:bawabtalsharq/Screens/sub_categories_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../Utils/images.dart';
@@ -10,23 +9,23 @@ class AllCategories extends StatefulWidget {
   _AllCategoriesState createState() => _AllCategoriesState();
 }
 
-class _AllCategoriesState extends State<AllCategories> {
+class _AllCategoriesState extends State<AllCategories>
+    with TickerProviderStateMixin {
   ScrollController _scrollController = ScrollController();
+  List<Positioned> stackWidgets = List<Positioned>();
+  var appBarTitle = Strings().allCategories();
+  @override
+  void initState() {
+    stackWidgets.add(getMainCategoriesList(context));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawerScrimColor: Colors.transparent,
-      endDrawerEnableOpenDragGesture: false,
-      endDrawer: Container(
-        width: MediaQuery.of(context).size.width * 0.8,
-        margin: EdgeInsets.only(top: 124),
-        child: Drawer(
-          child: SubCategories(),
-        ),
-      ),
       appBar: appBarBuilder(
         actions: [Container()],
-        title: Strings().allCategories(),
+        title: appBarTitle,
         onBackPressed: () {
           Navigator.pop(context);
         },
@@ -39,47 +38,108 @@ class _AllCategoriesState extends State<AllCategories> {
         },
       ),
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.only(top: 32),
-          child: ListView.builder(
-            controller: _scrollController,
-            itemCount: 200,
-            scrollDirection: Axis.vertical,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  Scaffold.of(context).openEndDrawer();
-                },
-                child: Container(
-                  margin: EdgeInsets.all(10),
-                  child: Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.green[200]),
-                        child: Padding(
-                          padding: const EdgeInsets.all(2),
-                          child: Image.asset(
-                            cold_drinks,
-                            height: 45,
-                            width: 45,
-                          ),
+        child: Stack(
+          children: stackWidgets,
+        ),
+      ),
+    );
+  }
+
+  Positioned getMainCategoriesList(BuildContext context) {
+    return Positioned(
+      child: Container(
+        margin: EdgeInsets.only(top: 25),
+        child: ListView.builder(
+          controller: _scrollController,
+          itemCount: 200,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  appBarTitle = 'SubCategory';
+                  stackWidgets.add(getSubCategoriesList(context));
+                });
+              },
+              child: Container(
+                margin: EdgeInsets.only(bottom: 15, left: 8, right: 8),
+                child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.green[200]),
+                      child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Image.asset(
+                          cold_drinks,
+                          height: 45,
+                          width: 45,
                         ),
                       ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Expanded(
-                        child: Text('Category Name'),
-                      ),
-                      Icon(Icons.navigate_next),
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: Text('Category Name'),
+                    ),
+                    Icon(Icons.navigate_next),
+                  ],
                 ),
-              );
-            },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Positioned getSubCategoriesList(BuildContext context) {
+    return Positioned(
+      right: 0,
+      width: MediaQuery.of(context).size.width * 0.83,
+      child: Container(
+        margin: EdgeInsets.only(top: 25),
+        decoration: BoxDecoration(
+          color: Colors.green[200],
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(8),
           ),
+        ),
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: 30,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.only(left: 15, right: 15, top: 20),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18),
+                        color: Colors.white),
+                    child: Padding(
+                      padding: const EdgeInsets.all(3),
+                      child: Image.asset(
+                        cold_drinks,
+                        height: 37,
+                        width: 37,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: Text('Sub Category Name'),
+                  ),
+                  Icon(Icons.navigate_next),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
