6ea1c2f9d6696975a37274a88c2d7ad651321a4e
Asmaa Tarek <asmaatarek93@gmail.com>
2021-03-22 19:53:44 +0200

    refactoring

--- bawabtalsharq/lib/Screens/splash_screen.dart
+++ bawabtalsharq/lib/Screens/splash_screen.dart
@@ -1,13 +1,205 @@
-import 'package:bawabtalsharq/Utils/size_config.dart';
-import 'package:bawabtalsharq/widgets/body.dart';
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-class SplashScreen extends StatelessWidget {
+import 'package:bawabtalsharq/Utils/images.dart';
+import 'package:bawabtalsharq/Utils/size_config.dart';
+import 'package:bawabtalsharq/Utils/strings.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  int imageIndex = 0;
+  PageController _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      body: Body(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/Background$imageIndex.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 70,
+                height: 30,
+                margin: EdgeInsets.only(top: 10, bottom: 10, right: 12),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    print('language');
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'EN',
+                        style: TextStyle(color: Colors.black54, fontSize: 18),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Icon(
+                          Icons.arrow_drop_down,
+                          size: 25,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(37),
+                      topRight: Radius.circular(37),
+                    ),
+                  ),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      PageView(
+                        controller: _pageController,
+                        pageSnapping: true,
+                        scrollDirection: Axis.horizontal,
+                        onPageChanged: (page) {
+                          setState(() {
+                            // print(page);
+                            imageIndex = page;
+                          });
+                        },
+                        children: [
+                          pageBuilder(intro1, Strings().intro1Title(),
+                              Strings().intro1SubTitle()),
+                          pageBuilder(intro2, Strings().intro2Title(),
+                              Strings().intro2SubTitle()),
+                          pageBuilder(intro3, Strings().intro3Title(),
+                              Strings().intro3SubTitle())
+                        ],
+                      ),
+                      Positioned(
+                        top: MediaQuery.of(context).size.height * 0.62 + 35,
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: AnimatedSmoothIndicator(
+                            activeIndex: imageIndex,
+                            count: 3,
+                            effect: ExpandingDotsEffect(
+                                activeDotColor: imageIndex == 0
+                                    ? Colors.green
+                                    : (imageIndex == 1
+                                        ? Colors.blueAccent
+                                        : Colors.redAccent),
+                                dotColor: imageIndex == 0
+                                    ? Colors.green.withOpacity(0.3)
+                                    : (imageIndex == 1
+                                        ? Colors.blueAccent.withOpacity(0.3)
+                                        : Colors.redAccent.withOpacity(0.3))),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 30,
+                        bottom: 30,
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                bottom: 20,
+                                left: 25,
+                              ),
+                              child: GestureDetector(
+                                onTap: () {
+                                  imageIndex < 2
+                                      ? _pageController
+                                          .jumpToPage(imageIndex + 1)
+                                      : print('login');
+                                },
+                                child: Text(
+                                  Strings().next(),
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 15),
+                              child: Icon(
+                                Icons.arrow_forward,
+                                size: 17,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container pageBuilder(String imageName, String title, String subTitle) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset(
+            imageName,
+            fit: BoxFit.fill,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.5,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Text(
+              title,
+              style: TextStyle(
+                  fontFamily: "Segoe UI",
+                  fontWeight: FontWeight.w700,
+                  fontSize: 30),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            child: Text(
+              subTitle,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: "Segoe UI",
+                fontWeight: FontWeight.w400,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- bawabtalsharq/lib/Utils/strings.dart
+++ bawabtalsharq/lib/Utils/strings.dart
@@ -35,6 +35,97 @@ class Strings {
 
 // Start Moaaz
 
+  String intro1Title() {
+    switch (Constants.languageId) {
+      case languages.Arabic:
+        return StringsAR.intro1Title;
+        break;
+      case languages.Indian:
+        return StringsINR.intro1Title;
+        break;
+      default:
+        return StringsEN.intro1Title;
+    }
+  }
+
+  String intro1SubTitle() {
+    switch (Constants.languageId) {
+      case languages.Arabic:
+        return StringsAR.intro1SubTitle;
+        break;
+      case languages.Indian:
+        return StringsINR.intro1SubTitle;
+        break;
+      default:
+        return StringsEN.intro1SubTitle;
+    }
+  }
+
+  String intro2Title() {
+    switch (Constants.languageId) {
+      case languages.Arabic:
+        return StringsAR.intro2Title;
+        break;
+      case languages.Indian:
+        return StringsINR.intro2Title;
+        break;
+      default:
+        return StringsEN.intro2Title;
+    }
+  }
+
+  String intro2SubTitle() {
+    switch (Constants.languageId) {
+      case languages.Arabic:
+        return StringsAR.intro2SubTitle;
+        break;
+      case languages.Indian:
+        return StringsINR.intro2SubTitle;
+        break;
+      default:
+        return StringsEN.intro2SubTitle;
+    }
+  }
+
+  String intro3Title() {
+    switch (Constants.languageId) {
+      case languages.Arabic:
+        return StringsAR.intro3Title;
+        break;
+      case languages.Indian:
+        return StringsINR.intro3Title;
+        break;
+      default:
+        return StringsEN.intro3Title;
+    }
+  }
+
+  String intro3SubTitle() {
+    switch (Constants.languageId) {
+      case languages.Arabic:
+        return StringsAR.intro3SubTitle;
+        break;
+      case languages.Indian:
+        return StringsINR.intro3SubTitle;
+        break;
+      default:
+        return StringsEN.intro3SubTitle;
+    }
+  }
+
+  String next() {
+    switch (Constants.languageId) {
+      case languages.Arabic:
+        return StringsAR.next;
+        break;
+      case languages.Indian:
+        return StringsINR.next;
+        break;
+      default:
+        return StringsEN.next;
+    }
+  }
+
 // end Moaaz
 
 // Start Islam
