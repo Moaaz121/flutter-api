c3e934112a02bfe79d8b494409307920af01f3aa
Asmaa Tarek <asmaatarek93@gmail.com>
2021-03-22 14:56:53 +0200

    review and refactor

--- bawabtalsharq/lib/Screens/main_srceen.dart
+++ bawabtalsharq/lib/Screens/main_srceen.dart
@@ -1,11 +1,10 @@
-
 import 'package:bawabtalsharq/Screens/profile_screen.dart';
 import 'package:bawabtalsharq/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 import 'feature_screen.dart';
 import 'home_screen.dart';
-import 'notificationScreen.dart';
+import 'notification_screen.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -16,34 +15,30 @@ class _MainScreenState extends State<MainScreen> {
   int currentPage = 0;
   PageController controller = PageController(initialPage: 0, keepPage: true);
 
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       top: false,
-
       child: Scaffold(
         extendBody: true,
-        bottomNavigationBar: bottomAppBar((int position){
-
-          controller.animateToPage(position, duration: Duration(milliseconds: 200), curve: Curves.linear);
-
-        },controller,currentPage),
+        bottomNavigationBar: bottomAppBar((int position) {
+          controller.animateToPage(position,
+              duration: Duration(milliseconds: 200), curve: Curves.linear);
+        }, controller, currentPage),
         body: PageView(
-            controller: controller,
-            children: [
-              HomeScreen(),
-              FeatureScreen(),
-              NotificationScreen(),
-              ProfileScreen(),
-            ],
-            onPageChanged: (page){
-              setState(() {
-                currentPage = page;
-              });
-            },
-          ),
-
+          controller: controller,
+          children: [
+            HomeScreen(),
+            FeatureScreen(),
+            NotificationScreen(),
+            ProfileScreen(),
+          ],
+          onPageChanged: (page) {
+            setState(() {
+              currentPage = page;
+            });
+          },
+        ),
       ),
     );
   }
