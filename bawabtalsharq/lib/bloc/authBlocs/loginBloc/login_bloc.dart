c5bb4df56e2490ba5efa524fbfd432682d88e46d
Ahmed Bahaa <61113692+ahmedbahaa123@users.noreply.github.com>
2021-04-27 12:47:32 +0200

    blocs

--- bawabtalsharq/lib/bloc/authBlocs/loginBloc/login_bloc.dart
+++ bawabtalsharq/lib/bloc/authBlocs/loginBloc/login_bloc.dart
@@ -1,12 +1,6 @@
 import 'dart:async';
-<<<<<<< HEAD:bawabtalsharq/lib/bloc/loginBloc/login_bloc.dart
-
-import 'package:bawabtalsharq/bloc/loginBloc/login_event.dart';
-import 'package:bawabtalsharq/bloc/loginBloc/login_state.dart';
-=======
 import 'package:bawabtalsharq/bloc/authBlocs/loginBloc/login_event.dart';
 import 'package:bawabtalsharq/bloc/authBlocs/loginBloc/login_state.dart';
->>>>>>> 989128bed71d2c63619e8978f98a07bbddaa437e:bawabtalsharq/lib/bloc/authBlocs/loginBloc/login_bloc.dart
 import 'package:bawabtalsharq/repo/auth_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
