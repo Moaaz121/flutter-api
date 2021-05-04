9b494d455b860084dceb3dcd0e5357ece96c0bba
Ahmed Bahaa <61113692+ahmedbahaa123@users.noreply.github.com>
2021-05-04 15:06:01 +0200

    location padding

--- bawabtalsharq/lib/Screens/profile/contact_us/location_dialog.dart
+++ bawabtalsharq/lib/Screens/profile/contact_us/location_dialog.dart
@@ -10,51 +10,58 @@ import 'package:flutter/material.dart';
 Widget locationDialog(BuildContext context, String location) {
   return Center(
     child: Container(
-      height: 70,
+      height: 80,
       width: MediaQuery.of(context).size.width,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          SizedBox(
+            height: 16,
+          ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Flexible(
-                child: Flex(
-                  direction: Axis.horizontal,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
+              child: Flex(
+                direction: Axis.horizontal,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
                       Icons.location_on_rounded,
                       size: 14,
                     ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    buildText(
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: buildText(
                       location,
                       12,
                       fontWeight: FontWeight.w600,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
-          Expanded(
-            child: GestureDetector(
-              onTap: () {
-                MapUtils.openMap(
-                  30.063216,
-                  31.341721,
-                );
-              },
-              child: buildText(Languages.of(context).getLocation, 12,
-                  color: orangeColor,
-                  fontWeight: FontWeight.w600,
-                  textDecoration: TextDecoration.underline,
-                  decorationColor: orangeColor),
-            ),
+          GestureDetector(
+            onTap: () {
+              MapUtils.openMap(
+                30.063216,
+                31.341721,
+              );
+            },
+            child: buildText(Languages.of(context).getLocation, 12,
+                color: orangeColor,
+                fontWeight: FontWeight.w600,
+                textDecoration: TextDecoration.underline,
+                decorationColor: orangeColor),
           ),
+          SizedBox(
+            height: 16,
+          )
         ],
       ),
       margin: EdgeInsets.only(bottom: 50, left: 16, right: 16),
