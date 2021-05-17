2738bf907d1d9a744e1d003ec3a7497eb758cfa1
MohamedMosadik <59214859+MohamedMosadik@users.noreply.github.com>
2021-05-17 12:34:24 +0200

    update Category

---
+++ bawabtalsharq/lib/Model/country_model.dart
@@ -0,0 +1,97 @@
+// To parse this JSON data, do
+//
+//     final countryModel = countryModelFromJson(jsonString);
+
+import 'dart:convert';
+
+CountryModel countryModelFromJson(String str) =>
+    CountryModel.fromJson(json.decode(str));
+
+String countryModelToJson(CountryModel data) => json.encode(data.toJson());
+
+class CountryModel {
+  CountryModel({
+    this.code,
+    this.status,
+    this.msg,
+    this.data,
+  });
+
+  int code;
+  String status;
+  String msg;
+  List<Datum> data;
+
+  factory CountryModel.fromJson(Map<String, dynamic> json) => CountryModel(
+        code: json["code"],
+        status: json["status"],
+        msg: json["msg"],
+        data: List<Datum>.from(json["data"].map((x) => Datum.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "code": code,
+        "status": status,
+        "msg": msg,
+        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+      };
+}
+
+class Datum {
+  Datum({
+    this.code,
+    this.region,
+    this.lat,
+    this.lon,
+    this.country,
+  });
+
+  String code;
+  Region region;
+  String lat;
+  String lon;
+  String country;
+
+  factory Datum.fromJson(Map<String, dynamic> json) => Datum(
+        code: json["code"],
+        region: regionValues.map[json["region"]],
+        lat: json["lat"],
+        lon: json["lon"],
+        country: json["country"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "code": code,
+        "region": regionValues.reverse[region],
+        "lat": lat,
+        "lon": lon,
+        "country": country,
+      };
+}
+
+enum Region { AS, EU, AF, AU, LA, AN, NA, EMPTY }
+
+final regionValues = EnumValues({
+  "AF": Region.AF,
+  "AN": Region.AN,
+  "AS": Region.AS,
+  "AU": Region.AU,
+  "": Region.EMPTY,
+  "EU": Region.EU,
+  "LA": Region.LA,
+  "NA": Region.NA
+});
+
+class EnumValues<T> {
+  Map<String, T> map;
+  Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    if (reverseMap == null) {
+      reverseMap = map.map((k, v) => new MapEntry(v, k));
+    }
+    return reverseMap;
+  }
+}
