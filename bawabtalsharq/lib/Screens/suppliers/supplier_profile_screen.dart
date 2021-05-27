a7f1e414abe1209265670773c97254e8faf06129
MohamedMosadik <59214859+MohamedMosadik@users.noreply.github.com>
2021-05-27 18:11:56 +0200

    AnimanishButtonChatSuppliers

--- bawabtalsharq/lib/Screens/suppliers/supplier_profile_screen.dart
+++ bawabtalsharq/lib/Screens/suppliers/supplier_profile_screen.dart
@@ -1,4 +1,3 @@
-import 'package:bawabtalsharq/Screens/suppliers/Waves.dart';
 import 'package:bawabtalsharq/Utils/Localization/Language/Languages.dart';
 import 'package:bawabtalsharq/Utils/Localization/LanguageHelper.dart';
 import 'package:bawabtalsharq/Utils/images.dart';
@@ -8,11 +7,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'CirclPainter.dart';
+import 'Waves.dart';
 
 class SupplierProfile extends StatefulWidget {
   const SupplierProfile({
     Key key,
-    this.size = 80.0,
+    this.size = 100.0,
     this.color = Colors.red,
     this.onPressed,
     @required this.child,
@@ -83,169 +83,169 @@ class _SupplierProfileState extends State<SupplierProfile>
           children: [
             SizedBox(
               height: 220,
-              child: CustomPaint(
-                painter: CirclePainter(
-                  _controller,
-                  color: widget.color,
-                ),
-                child: Stack(
-                  children: [
-                    Container(
-                      height: 120,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(supplierBanner),
-                          colorFilter: new ColorFilter.mode(
-                              Colors.black.withOpacity(0.5), BlendMode.darken),
-                          fit: BoxFit.fill,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 120,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(supplierBanner),
+                        colorFilter: new ColorFilter.mode(
+                            Colors.black.withOpacity(0.5), BlendMode.darken),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    child: Positioned(
+                      child: Column(
+                        children: [
+                          Divider(
+                            indent: 80,
+                            endIndent: 85,
+                            thickness: 1.5,
+                            color: Colors.white,
+                          ),
+                          buildText(
+                            Languages.of(context).bawabtAlsharq,
+                            30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          Divider(
+                            indent: 80,
+                            endIndent: 85,
+                            thickness: 1.5,
+                            color: Colors.white,
+                          ),
+                          buildText(
+                            Languages.of(context).feature,
+                            20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned.directional(
+                    textDirection: Directionality.of(context),
+                    top: 20,
+                    start: 10,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: CircleAvatar(
+                        radius: 9,
+                        backgroundColor: Colors.white,
+                        child: Icon(
+                          LanguageHelper.isEnglish
+                              ? Icons.keyboard_arrow_left_outlined
+                              : Icons.keyboard_arrow_right_outlined,
+                          size: 15,
+                          color: Colors.black,
                         ),
                       ),
-                      child: Positioned(
-                        child: Column(
+                    ),
+                  ),
+                  Positioned.directional(
+                    textDirection: Directionality.of(context),
+                    top: 70,
+                    start: 30,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Container(
+                          width: 86,
+                          height: 86,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            border: Border.all(
+                              color: Colors.white,
+                            ),
+                            shape: BoxShape.rectangle,
+                            image: DecorationImage(
+                              image: AssetImage(mosadaq_img),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Divider(
-                              indent: 80,
-                              endIndent: 85,
-                              thickness: 1.5,
-                              color: Colors.white,
-                            ),
-                            buildText(
-                              Languages.of(context).bawabtAlsharq,
-                              30,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w400,
-                            ),
-                            Divider(
-                              indent: 80,
-                              endIndent: 85,
-                              thickness: 1.5,
-                              color: Colors.white,
-                            ),
-                            buildText(
-                              Languages.of(context).feature,
-                              20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w400,
-                            ),
+                            buildText('Bahaa Robert', 12),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.access_time_rounded,
+                                  size: 12,
+                                ),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                buildText(
+                                  'Member since: 2020',
+                                  8,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                )
+                              ],
+                            )
                           ],
                         ),
-                      ),
+                      ],
                     ),
-                    Positioned.directional(
-                      textDirection: Directionality.of(context),
-                      top: 20,
-                      start: 10,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: CircleAvatar(
-                          radius: 9,
-                          backgroundColor: Colors.white,
-                          child: Icon(
-                            LanguageHelper.isEnglish
-                                ? Icons.keyboard_arrow_left_outlined
-                                : Icons.keyboard_arrow_right_outlined,
-                            size: 15,
-                            color: Colors.black,
-                          ),
+                  ),
+                  Positioned.directional(
+                    textDirection: Directionality.of(context),
+                    top: 170,
+                    start: 20,
+                    child: Row(
+                      children: [
+                        iconOfSupplier(Icons.location_on, () {}),
+                        SizedBox(
+                          width: 30,
                         ),
-                      ),
+                        iconOfSupplier(Icons.phone, () {}),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        iconOfSupplier(Icons.email_outlined, () {}),
+                      ],
                     ),
-                    Positioned.directional(
-                      textDirection: Directionality.of(context),
-                      top: 70,
-                      start: 30,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Container(
-                            width: 86,
-                            height: 86,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              border: Border.all(
-                                color: Colors.white,
-                              ),
-                              shape: BoxShape.rectangle,
-                              image: DecorationImage(
-                                image: AssetImage(mosadaq_img),
-                                fit: BoxFit.fill,
-                              ),
-                            ),
+                  ),
+                  Positioned.directional(
+                    textDirection: Directionality.of(context),
+                    top: 135,
+                    end: 30,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        CustomPaint(
+                          painter: CirclePainter(
+                            _controller,
+                            color: widget.color,
                           ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              buildText('Bahaa Robert', 12),
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.access_time_rounded,
-                                    size: 12,
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  buildText(
-                                    'Member since: 2020',
-                                    8,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                  SizedBox(
-                                    height: 15,
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Positioned.directional(
-                      textDirection: Directionality.of(context),
-                      top: 170,
-                      start: 20,
-                      child: Row(
-                        children: [
-                          iconOfSupplier(Icons.location_on, () {}),
-                          SizedBox(
-                            width: 30,
-                          ),
-                          iconOfSupplier(Icons.phone, () {}),
-                          SizedBox(
-                            width: 30,
-                          ),
-                          iconOfSupplier(Icons.email_outlined, () {}),
-                        ],
-                      ),
-                    ),
-                    Positioned.directional(
-                      textDirection: Directionality.of(context),
-                      top: 135,
-                      end: 30,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          chatButtonAnimation(() {},
+                          child: chatButtonAnimation(() {},
                               size: MediaQuery.of(context).size.width * 0.1,
-                              sizeIcon: 8,
+                              sizeIcon: 7,
                               padOfIcon: 6),
-                          buildText(
-                            Languages.of(context).startChat,
-                            10,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                        ),
+                        buildText(
+                          Languages.of(context).startChat,
+                          10,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
             Container(
@@ -279,7 +279,7 @@ class _SupplierProfileState extends State<SupplierProfile>
   }
 
   Widget chatButtonAnimation(Function _function,
-      {double size = 45, double sizeIcon = 10, double padOfIcon = 16}) {
+      {double size = 45, double sizeIcon = 15, double padOfIcon = 16}) {
     return Padding(
       padding: EdgeInsets.all(padOfIcon),
       child: SizedBox(
@@ -288,25 +288,35 @@ class _SupplierProfileState extends State<SupplierProfile>
         child: ClipRRect(
           borderRadius: BorderRadius.circular(widget.size),
           child: DecoratedBox(
-            decoration: BoxDecoration(
-              gradient: RadialGradient(
-                colors: <Color>[
-                  widget.color,
-                  Color.lerp(widget.color, Colors.black, .05)
-                ],
-              ),
-            ),
-            child: ScaleTransition(
-                scale: Tween(begin: 0.95, end: 1.0).animate(
-                  CurvedAnimation(
-                    parent: _controller,
-                    curve: CurveWave(),
+            decoration: BoxDecoration(),
+            child: ClipOval(
+              child: Material(
+                shadowColor: orangeShadowColor,
+                color: Colors.white,
+                child: InkWell(
+                  onTap: _function,
+                  splashColor: orangeColor.withOpacity(0.4),
+                  highlightColor: orangeShadowColor,
+                  child: ScaleTransition(
+                    scale: Tween(begin: 0.85, end: 1.0).animate(
+                      CurvedAnimation(
+                        parent: _controller,
+                        curve: CurveWave(),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(sizeIcon),
+                      child: Image.asset(
+                        chat,
+                        width: 30,
+                        height: 30,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                   ),
                 ),
-                child: Icon(
-                  Icons.speaker_phone,
-                  size: 44,
-                )),
+              ),
+            ),
           ),
         ),
         // Container(
