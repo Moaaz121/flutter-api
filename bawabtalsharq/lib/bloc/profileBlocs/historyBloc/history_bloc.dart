f85e0f8413527491ca92dc97d2e6d5fa03b2347d
Ahmed Bahaa <61113692+ahmedbahaa123@users.noreply.github.com>
2021-06-02 17:11:32 +0200

    history_bloc.dart

--- bawabtalsharq/lib/bloc/profileBlocs/historyBloc/history_bloc.dart
+++ bawabtalsharq/lib/bloc/profileBlocs/historyBloc/history_bloc.dart
@@ -1,30 +1,21 @@
 import 'dart:async';
 
-import 'package:bawabtalsharq/Services/checkIntrernetConnectivity.dart';
 import 'package:bawabtalsharq/bloc/profileBlocs/historyBloc/history_event.dart';
 import 'package:bawabtalsharq/bloc/profileBlocs/historyBloc/history_state.dart';
 import 'package:bawabtalsharq/repo/profile_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HistoryBloc extends Bloc<HistoryEvent, HistoryState> {
-  InternetConnection internetConnection = InternetConnection();
-
   @override
   Stream<HistoryState> mapEventToState(
     HistoryEvent event,
   ) async* {
-    bool connected = await internetConnection.isConnected();
-    print('Connection' + '  ' + connected.toString());
     if (event is GetHistoryEvent) {
       yield HistoryLoadingState();
       try {
-        if (connected) {
-          var response =
-              await ProfileRepo().getHistory(event.userId, event.apiKey);
-          yield HistoryLoadedState(response: response);
-        } else {
-          yield HistoryNoInternetState();
-        }
+        var response =
+            await ProfileRepo().getHistory(event.userId, event.apiKey);
+        yield HistoryLoadedState(response: response);
       } catch (e) {
         yield HistoryErrorState(message: e.toString());
       }
