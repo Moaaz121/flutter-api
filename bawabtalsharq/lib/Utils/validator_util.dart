12aa6e345e433fe325507f2576d58010d938aa57
Ahmed Bahaa <61113692+ahmedbahaa123@users.noreply.github.com>
2021-06-02 12:16:22 +0200

    handling notification if user equal to null
    reduce password validator

--- bawabtalsharq/lib/Utils/validator_util.dart
+++ bawabtalsharq/lib/Utils/validator_util.dart
@@ -5,7 +5,7 @@ bool emailValidator(String email) {
 }
 
 bool passwordValidator(String password) {
-  return RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
+  return RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$')
       .hasMatch(password);
 }
 
