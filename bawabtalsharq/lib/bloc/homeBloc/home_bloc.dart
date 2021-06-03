8c451923819f846d8ebd7cd4c688395eb35cfa41
kareemhassanein <dev.kareemhassanein@gmail.com>
2021-06-03 18:42:34 +0200

    no internet at home

--- bawabtalsharq/lib/bloc/homeBloc/home_bloc.dart
+++ bawabtalsharq/lib/bloc/homeBloc/home_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:bawabtalsharq/Services/checkIntrernetConnectivity.dart';
 import 'package:bawabtalsharq/bloc/homeBloc/home_event.dart';
 import 'package:bawabtalsharq/bloc/homeBloc/home_state.dart';
 import 'package:bawabtalsharq/repo/home_repo.dart';
@@ -10,13 +11,18 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   Stream<HomeState> mapEventToState(
     HomeEvent event,
   ) async* {
-    if (event is GetHomeData) {
-      yield HomeLoadingState();
-      var response = await HomeRepo().getHome();
+    var isConnected = await InternetConnection.isConnected2();
+    if (isConnected) {
+      if (event is GetHomeData) {
+        yield HomeLoadingState();
+        var response = await HomeRepo().getHome();
 
-      yield HomeLoadedState(homeResponse: response);
-    } else if (event is ResetState) {
-      yield HomeInitial();
+        yield HomeLoadedState(homeResponse: response);
+      } else if (event is ResetState) {
+        yield HomeInitial();
+      }
+    } else {
+      yield HomeNoInternetState();
     }
   }
 
