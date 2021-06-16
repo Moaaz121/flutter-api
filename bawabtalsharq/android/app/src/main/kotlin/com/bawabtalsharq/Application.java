e97399ca2b0411f5e08801c943a3cdde0276cb75
MohamedMosadik <59214859+MohamedMosadik@users.noreply.github.com>
2021-06-16 16:15:34 +0200

    editing ios

--- bawabtalsharq/android/app/src/main/kotlin/com/bawabtalsharq/Application.java
+++ bawabtalsharq/android/app/src/main/kotlin/com/bawabtalsharq/Application.java
@@ -3,22 +3,22 @@ package com.bawabtalsharq;
 import io.flutter.app.FlutterApplication;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingBackgroundService;
+//import io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingBackgroundService;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
 
 public class Application extends FlutterApplication implements PluginRegistry.PluginRegistrantCallback {
     @Override
     public void onCreate() {
         super.onCreate();
-        FlutterFirebaseMessagingBackgroundService.setPluginRegistrant(this);
+//        FlutterFirebaseMessagingBackgroundService.setPluginRegistrant(this);
     }
 
     @Override
     public void registerWith(PluginRegistry registry) {
-        io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingPlugin.registerWith(
-                registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
-
-        com.tekartik.sqflite.SqflitePlugin.registerWith(
-                registry.registrarFor("com.tekartik.sqflite.SqflitePlugin"));
+        FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+//        io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+        com.tekartik.sqflite.SqflitePlugin.registerWith(registry.registrarFor("com.tekartik.sqflite.SqflitePlugin"));
     }
 
 }
